a7dd2954526fee449dcd441a73eae4d7268823db
amiyakiran <amiyakiran@gmail.com>
2024-01-05 15:11:16 +0530

    await added

--- DB/connections.js
+++ DB/connections.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const connectionString = process.env.DATABASE
 
 //establish connection
-mongoose.connect(connectionString).then(()=>{
+await mongoose.connect(connectionString).then(()=>{
    console.log('Mongodb Atlas successfully connected with pfServer'); 
 }).catch((err)=>{
     console.log('Mongodb connection failed due to'+err); 
