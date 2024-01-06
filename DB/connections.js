4a972770776fa7764e746f5bfbcafbcfa8258e83
amiyakiran <amiyakiran@gmail.com>
2024-01-06 11:19:59 +0530

    options added

--- DB/connections.js
+++ DB/connections.js
@@ -3,9 +3,18 @@ const mongoose = require('mongoose')
 //get the connection string of mongodb
 const connectionString = process.env.DATABASE
 
+const options = {
+  serverSelectionTimeoutMS: 35000, // Set a higher value than the default 30000 (30 seconds)
+};
 //establish connection
-mongoose.connect(connectionString).then(()=>{
+mongoose.connect(connectionString,options).then(()=>{
    console.log('Mongodb Atlas successfully connected with pfServer'); 
 }).catch((err)=>{
     console.log('Mongodb connection failed due to'+err); 
 })
+
+/* const options = {
+  serverSelectionTimeoutMS: 15000, // Set a higher value than the default 30000 (30 seconds)
+};
+
+mongoose.connect('mongodb://localhost:27017/your_database', options); */
